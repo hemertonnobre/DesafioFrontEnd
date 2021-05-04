d80fa2bfa88fc63edcf8c2bcba8007a7af2a5d22
Hemerton Nobre <nobre_saxx@yahoo.com.br>
2021-05-04 20:36:16 -0300

    finalização do projecto desafio

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,56 @@
 
-window.addEventListener("load",function(){ 
-    if(localStorage.getItem("displayModal") == null || localStorage.getItem("displayModal") != "false" ){
+//Carrega o modal ao iniciar a paguina
+window.addEventListener("load", function () {
+    if (localStorage.getItem("displayModal") == null || localStorage.getItem("displayModal") != "false") {
         abrirModal();
-    } 
-   
+    }
+
 });
-function abrirModal(){
+
+//Valida os input e adiciona classes de verificação 
+document.querySelectorAll("input").forEach(element => {
+    element.addEventListener("keyup", function (event) {
+        let input = event.target;
+        if (input.checkValidity()) {
+            input.classList.remove("is-invalid");
+            input.classList.add("is-valid");
+        } else {
+            input.classList.remove("is-valid");
+            input.classList.add("is-invalid");
+        }
+    });
+});
+// Abre o modal.
+function abrirModal() {
     let modal = document.querySelector(".leed-modal");
-    modal.style.display="block";
+    modal.style.display = "block";
 }
-function fecharModal(){
+// Fecha o modal
+function fecharModal() {
     let modal = document.querySelector(".leed-modal");
-    modal.style.display="none";
+    modal.style.display = "none";
 }
-function enviarInfo(){
-    if(document.querySelector('#cache').checked){
-        localStorage.setItem("displayModal",false)
-    }  
-    fecharModal();
+//Valida o formulario, e verifica a opção do checkbox e salva e informação no cache.
+function enviarInfo() {
+    let form = document.querySelector(".needs-validation");
+    let nome = document.querySelector("#nome");
+    let email = document.querySelector("#email");
+    let cache = document.querySelector('#cache');
+    let valid = form.checkValidity();
+    if (!valid) {
+        if (!nome.checkValidity())
+            nome.classList.add("is-invalid");
+
+        if (!email.checkValidity())
+            email.classList.add("is-invalid");
+    } else {
+        if (cache.checked)
+            localStorage.setItem("displayModal", false)
+        fecharModal();
+    }
 }
-function limparStorage(){
+//limpa o cache ao clicar na logo.
+function limparStorage() {
     localStorage.clear();
-    
 }
+
